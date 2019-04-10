b54ed906c2ae8d14bdb4a997cc56b753cee63add
moondrvmedia <business@florian-hoppe.com>
2019-04-10 18:39:03 +0000

    changed the comment line

--- db_connnection.php
+++ db_connnection.php
@@ -4,7 +4,7 @@ $username = "dev_tatevik";
 $password = "E~iS=?A0bls517n@JiWbTINcEcbGgP9j";
 $dbname = "leadbase";
 
-// Create connection
+// Create connection to DB
 $conn = new mysqli($servername, $username, $password);
 
 // Check connection
